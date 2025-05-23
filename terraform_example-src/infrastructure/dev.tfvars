b76658c058bfea6930df03fe873d4aba39e687e1
Proye-2 <rhoyos161@soyudemedellin.edu.co>
2025-05-23 14:45:16 -0500

    Add files via upload

---
+++ terraform_example-src/infrastructure/dev.tfvars
@@ -0,0 +1,7 @@
+ami_id ="ami-0953476d60561c955" #Se remplama con ami ID de la instancia de AWS
+instance_type = "t2.micro"
+bucket_name= "proyecto-2-0953476d60561c955"
+aws_region="us-east-1"
+microservice_port =8000
+project_name = "proyecto-2"
+instance_count = 2
---
+++ terraform_example-src/infrastructure/user_data.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+yum update -y
+
+# Install Git
+yum install git -y
+
+# Install curl
+yum install curl -y
+
+# Install uv 
+sudo -u ec2-user -i -- bash -c 'curl -LsSf https://astral.sh/uv/install.sh | sh'
+
+# Clone repository
+git clone https://github.com/Proye-2/Proyecto-2/terraform_example-src.git
+cd terraform_example-src/
+uv sync
+
